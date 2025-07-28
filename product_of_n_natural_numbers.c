246fc144c7733a027acfcd0ade6e3eb44cd22f28
RickyPonting2006 <rickyponting2006@gmail.com>
2025-07-28 23:25:11 +0530

    Add files via upload

---
+++ product_of_n_natural_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    int product = 1;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    for(i = 1; i <= n; i++) {
+        product = product * i;
+    }
+
+    printf("Product of first %d natural numbers is: %d\n", n, product);
+
+    return 0;
+}
+
